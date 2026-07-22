0f703dba60d054471d5089c7e2c5a3f254407b70
Default Application User <ecp@mgl.ca>
2026-07-22 18:06:02 +0200

    ADC interfaces and integration of cordic and adc

--- src/lpc_core.sv
+++ src/lpc_core.sv
@@ -21,12 +21,280 @@ module lpc_core (
 	output logic adc_mosi,  // shift data input to adc
 	 input logic adc_miso 	// shift data output from adc
 );
+   	// Physical parameters
 
-	reg [15:0] state;
+
+	// TT tie-off (to be removed)
+	reg [15:0] xstate;
 	always_ff @(posedge clk) 
-		state <= ( reset ) ? 0 : state + &{ clk, reset, button, period_sw, timeout_sw, setup_sw, adc_miso };
-	assign { time_led,fault_led,run_led,pump_out,adc_ncs,adc_clk,adc_mosi } = state;
+		xstate <= ( reset ) ? 0 : xstate + &{ clk, reset, button, period_sw, timeout_sw, setup_sw, adc_miso };
+	assign { time_led, fault_led, run_led, pump_out } = xstate;
     
+	////////////////
+    // ADC inteface
+	////////////////
+
+	// ADC I/O Regs
+	logic ncs_io, clk_io, mosi_io, miso_io; 
+	// Output IO regs
+	always_ff @(posedge clk) adc_ncs <= ncs_io;
+	always_ff @(posedge clk) adc_clk <= clk_io;
+	always_ff @(posedge clk) adc_mosi<= mosi_io;
+	// Input IO reg
+	always_ff @(posedge clk) miso_io <= adc_miso;
+
+	// ADC serial interface
+	logic sdata, schan, sdval, sstrb;
+	adc_spi_master i_adcif (
+    	// Input clock,
+    	.clk	( clk ),
+    	.reset	( reset ),
+    	// External A/D Converter 
+    	// Assumed 1 external I/O reg on each.
+    	.ad_ncs	( ncs_io ),
+    	.ad_clk	( clk_io ),
+    	.ad_mosi( mosi_io ),
+    	.ad_miso( miso_io ),
+    	// ADC monitor outputs
+    	.dout( sdata ), // serial output
+    	.chan( schan ), // Indicate chan 0 or 1
+    	.dval( sdval ), // Indicates valid bit 
+    	.strb( sstrb ) // indicates start of channel 
+	);
+
+	////////////////
+	// Debounce
+	////////////////
+	
+	logic button_debounce; 
+	logic long_button;
+	forge_debounce #(48) i_bounc(.clk(clk),.reset(reset),.in(button),.out(button_debounce),.long(long_button));
+
+	////////////////
+    // 60Hz Coric 
+	////////////////
+	
+	// TODO tighten this down
+
+    logic [15:0] angle;
+    logic [15:0] sin_out, cos_out;
+    logic valid, busy, strobe;
+
+`define USE_CORDIC
+`ifdef USE_CORDIC
+    cordic_sincos_50000_core_20 i_cordic(
+        .clk( clk ),
+        .rst( reset ),
+        .start( strobe ),
+        .angle_in( angle ),
+        .sin_out ( sin_out ),
+        .cos_out ( cos_out ),
+        .valid( valid ),
+        .busy( busy )
+    );
+`endif
+	// Strobe to advance 
+	logic [11:0] strb_cnt;
+	always @(posedge clk) begin
+		strb_cnt <= ( reset || strb_cnt == 3199 ) ? 0 : strb_cnt + 1;
+		strobe   <= ( strb_cnt == 3199 ) ? 1'b1 : 1'b0;
+	end
+
+    // Count angle every start pulse (-25000 to 24999 )
+    // at 3Mhz (48Mhz/16) this gives us exactly 60 Hz grid freq
+	// with an advance of strobe of 3200 cycles, and advance of 200 gives 60Hz
+	// TODO make sure to reduce this logic
+
+    reg polarity;
+    reg pdir;
+    always @(posedge clk) begin
+        if( reset ) begin
+            angle <= 12500;
+            polarity <= 0;
+            pdir <= 0;
+        end else begin
+            if( strobe ) begin
+                angle <= angle + (( pdir ) ? 200 : -200);
+                polarity <= ( angle == 12499 && pdir == 1 ) ? ~polarity : polarity;
+                pdir <= ( pdir == 0 && angle == 1 ) ? 1 : ( pdir == 1 && angle == 12499 ) ? 0 : pdir;
+            end
+        end
+    end
+   // Multiply cos by 3: to nicely fill dynamic range
+    wire signed [11:0] cos3x;
+    wire signed [11:0] sin3x;
+
+`ifdef USE_CORDIC
+    assign cos3x = cos_out[15-:12] + ( cos_out[15-:12] >>> 1 );
+    assign sin3x = sin_out[15-:12] + ( sin_out[15-:12] >>> 1 );
+`endif
+
+//`define MAKE_ROM
+`ifdef MAKE_ROM
+    /////////////////////
+    // Build a rom
+    reg [8:0] cos_rom[31:0];
+    initial for( int ii = 0; ii < 32; ii++ )
+        cos_rom[ii] <= 12'sd0;
+    reg [15:0] prev_angle;
+    always @(posedge clk) begin
+        prev_angle <= angle;
+        if( strobe && !angle[15] )
+            if( prev_angle[8:0] == (1<<8) )  cos_rom[prev_angle[13-:5]] <= cos3x[10:2];
+    end
+    always @(posedge clk) begin
+        if( strobe && angle == 100 && pdir == 1 && polarity == 0 )
+            for( int ii = 0; ii < 32; ii++ )
+            $display("cos_rom[%0d] = 9'd%0d;", ii, cos_rom[ii] );
+    end
+    ///////////////////
+`endif
+
+`ifndef USE_CORDIC // if not cordic, then ROM
+    reg [8:0] cos_rom [31:0];
+    initial begin
+cos_rom[0] = 9'd385;
+cos_rom[1] = 9'd384;
+cos_rom[2] = 9'd380;
+cos_rom[3] = 9'd375;
+cos_rom[4] = 9'd369;
+cos_rom[5] = 9'd361;
+cos_rom[6] = 9'd352;
+cos_rom[7] = 9'd341;
+cos_rom[8] = 9'd329;
+cos_rom[9] = 9'd315;
+cos_rom[10] = 9'd300;
+cos_rom[11] = 9'd284;
+cos_rom[12] = 9'd267;
+cos_rom[13] = 9'd249;
+cos_rom[14] = 9'd229;
+cos_rom[15] = 9'd209;
+cos_rom[16] = 9'd187;
+cos_rom[17] = 9'd166;
+cos_rom[18] = 9'd143;
+cos_rom[19] = 9'd119;
+cos_rom[20] = 9'd96;
+cos_rom[21] = 9'd72;
+cos_rom[22] = 9'd47;
+cos_rom[23] = 9'd22;
+cos_rom[24] = 9'd0;
+cos_rom[25] = 9'dx;
+cos_rom[26] = 9'dx;
+cos_rom[27] = 9'dx;
+cos_rom[28] = 9'dx;
+cos_rom[29] = 9'dx;
+cos_rom[30] = 9'dx;
+cos_rom[31] = 9'dx;
+    end
+    assign valid = 1;
+    wire [8:0] read;
+    assign read = cos_rom[angle[13-:5]];
+    assign cos3x = { 1'b0, read, 2'b00 };
+`endif // ROM not CORDIC
+
+    // Correct Polarity (just negate)
+    reg signed [11:0] sin, cos;
+    always @(posedge clk) begin
+        if( reset ) begin
+            sin<= 0;
+            cos<= 0;
+        end else if( valid ) begin
+            sin   <= ( polarity ) ? ~sin3x : sin3x; // use cos as it aligns with polarity
+            cos   <= ( polarity ) ? ~cos3x : cos3x; // use cos as it aligns with polarity
+        end
+    end
+
+	////////////////
+    // RMS Compute
+	////////////////
+
+	// Serial MACC
+
+	////////////////
+    // LPC Control
+	////////////////
+
+	// 24hr/6hr period timer
+
+	// With increasingb breathing rate LED
+
+	// Over Current Logic
+
+	// Low Current Logic
+
+	// Timeout Logic
+
+	// Pump Cycle Logic
+
+	// Setup Mode
+
+	
+endmodule
+
+module forge_debounce(
+    input clk,
+    input reset,
+    input in,
+    output out, // fixed pulse 15ms after 5ms pressure
+    output long // after fire held for > 2/3 sec, until release
+    );
+
+    parameter CLOCK_FREQ_MHZ = 48;
+    localparam CYC_PER_MS = CLOCK_FREQ_MHZ * 1000; // 1 Ms count time
+    localparam CYC_LONG   = ( CLOCK_FREQ_MHZ * 2 / 3 ) * 'h100000;
+
+    logic [25:0] count1 = 0; // total 1.3 sec
+    logic [22:0] count0 = 0;
+    logic [2:0] meta;
+    logic       inm;
+
+
+    always_ff @(posedge clk) { inm, meta } <= { meta, in };
+
+    // State Machine    
+    localparam S_IDLE       = 0;
+    localparam S_WAIT_PRESS = 1;
+    localparam S_WAIT_PULSE = 2;
+    localparam S_WAIT_LONG  = 3;
+    localparam S_LONG       = 4;
+    localparam S_WAIT_OFF   = 5;
+    localparam S_WAIT_LOFF  = 6;
+    logic [2:0] state = S_IDLE;
+    always_ff @(posedge clk) begin
+        if( reset ) begin
+            state <= S_IDLE;
+        end else begin
+            case( state )
+                S_IDLE       :  state <= ( inm ) ? S_WAIT_PRESS : S_IDLE;
+                S_WAIT_PRESS :  state <= (!inm ) ? S_IDLE       : (count1 == ( 5  * CYC_PER_MS )) ? S_WAIT_PULSE : S_WAIT_PRESS;    // 5 msec debounce on
+                S_WAIT_PULSE :  state <=                          (count1 == ( 25 * CYC_PER_MS )) ? S_WAIT_LONG  : S_WAIT_PULSE;    // 25 msec pusle
+                S_WAIT_LONG  :  state <= (!inm ) ? S_WAIT_OFF   : (count1 >=          CYC_LONG  ) ? S_LONG       : S_WAIT_LONG;         // 0.66 sec long
+                S_LONG       :  state <= (!inm ) ? S_WAIT_LOFF  :  S_LONG;
+                S_WAIT_OFF   :  state <= ( inm ) ? S_WAIT_LONG  : (count0 == ( 100 * CYC_PER_MS)) ? S_IDLE       : S_WAIT_OFF;      // 100 mses debounce off
+                S_WAIT_LOFF  :  state <= ( inm ) ? S_LONG       : (count0 == ( 100 * CYC_PER_MS)) ? S_IDLE       : S_WAIT_LOFF;
+                default: state <= S_IDLE;
+            endcase
+        end
+    end
+
+    assign out = (state == S_WAIT_PULSE) ? 1'b1 : 1'b0;
+    assign long = (state == S_LONG || state == S_WAIT_LOFF) ? 1'b1 : 1'b0;
+
+    // Counters
+    always_ff @(posedge clk) begin
+        if( reset ) begin
+            count0 <= 0;
+            count1 <= 0;
+        end else begin
+            count0 <= ( state == S_WAIT_OFF  ||
+                        state == S_WAIT_LOFF ) ? (count0 + 1) : 0; // count when low waiting
+            count1 <= ( state == S_IDLE      ) ? 0            : (count1 + 1);
+        end
+    end
 
 endmodule
+
+
+
+
 	
--- src/spi_adc.sv
+++ src/spi_adc.sv
@@ -1,21 +1,90 @@
 // vim: ts=4:
 
+// Interface modules for MCP3202 12-bit spi adc.
+// Master, Monitor, Simulator. With these 3 blocks all anchored on the 4 I/O pins (spi)
+// They are targetted at the LPC chip. They will be used for:
+// emulation, monitoring, simlation fpga accelleration, asic bist?, chip testers and verilator testbenches.
+
+// This is the master interface on chip, intended to talk to I/O pads
 // Talks to the SPI ADC and strobes the data onto the bus
-module spi_adc_in
+// Runs continuous. Alternating reads of channel 0, then channel 1, and repeat
+// Sample rate is 15K each (total 30K). The ad_clk runs at 600Khzi. 40 ad_clk cycles to sample 0 and 1.
+// Area is to be minimized. A bit serial (MSB) output is used
+module adc_spi_master
 (
 	// Input clock,
 	input wire clk,
 	input wire reset,
 	
-	// External A/D Converters (2.5v)
-	output reg  ad_cs,
-	input  wire  [1:0] ad_sdata,
+	// External A/D Converter 
+	// Assumed 1 external I/O reg on each.
+	output logic ad_ncs,
+	output logic ad_clk,
+	output logic ad_mosi,
+	input  logic ad_miso,
 	
 	// ADC monitor outputs
-	output wire [11:0] ad_out0,
-	output wire [11:0] ad_out1,
-	output reg ad_strobe
+	output logic dout, // serial output
+	output logic chan, // Indicate chan 0 or 1
+	output logic dval, // Indicates valid bit 
+    output logic strb // indicates start next chan, chan valid, Coincident with dval tbd.
 );
 
+	// Tie off TT outs for now
+	assign ad_ncs = 0;
+	assign ad_clk = 0;
+	assign ad_mosi = 0;
+
+
+endmodule
+
+// This the monitor interface. It can montitor internal or external interfaces
+// The interface can be asychous. The interface is oversampled and decoded, with
+// parallel 12 bit output. and valid strobe
+// External registers for async crossing are assumed. Should use I/O regs for chip monitoring.
+// Also use to monitor internal interfaced in the case of emulation or simulation.
+module adc_spi_monitor
+(
+	// Input clock,
+	input wire clk,
+	input wire reset,
+	
+	// A/D Converter interface
+	input  logic ad_ncs,
+	input  logic ad_clk,
+	input  logic ad_mosi,
+	input  logic ad_miso,
+	
+	// ADC monitor outputs
+	output logic [11:0] dout, // parallel output
+	output logic chan, // Indicate chan 0 or 1
+    output logic strb // indicates start next chan, chan valid, Coincident with dval tbd.
+);
+endmodule
+
+// This is the simulated ADC (MCP3202), Not general purpose
+// SImualted only enough to work for this applicaiton.
+// It has input ports for the parallel 12 bit data ports (0, 1)
+// The external interface may be asycnronous, however latency matters somewhat
+// This interface assumes exactly 1 ff between the core and the chip pin (IO flop) for all 4 signals.
+module adc_spi_simulate
+(
+	// Input clock,
+	input wire clk,
+	input wire reset,
+	
+	// A/D Converter spi interface
+	// Assume 1 external I/O Flop per signal
+	input  logic ad_ncs,
+	input  logic ad_clk,
+	input  logic ad_mosi,
+	output logic ad_miso,
+	
+	// ADC simulation inteface
+	input logic [11:0] din0,
+	input logic [11:0] din1,
+	output logic strb0, // din0 was sampled
+	output logic strb1  // din1 was sampled
+);
 endmodule
 
